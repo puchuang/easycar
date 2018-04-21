396386c398d86862044eb711c6bda70c6c33e929
kaige6612 <1134251424@qq.com>
2018-04-22 00:50:44 +0800

    封装更新和删除的公用方法

--- src/main/java/com/easycar/service/common/CommonService.java
+++ src/main/java/com/easycar/service/common/CommonService.java
@@ -4,6 +4,7 @@ import com.easycar.dao.DaoSupport;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service("commonService")
@@ -11,12 +12,53 @@ public class CommonService{
     @Resource(name = "daoSupport")
     private DaoSupport dao;
 
-    public Map<String,String> findByType(String type) {
+    /**
+     * 更新单个状态的公用方法
+     * @param tableName
+     * @param statusName
+     * @param resultCode
+     * @return
+     */
+    public boolean updateStatus(String tableName,String statusName,String resultCode,String queryColumn,String queryLimit) {
+        boolean result = false;
+        Map<String,String> paramMap = new HashMap<String, String>();
+        paramMap.put("tableName",tableName);
+        paramMap.put("statusName",statusName);
+        paramMap.put("statusResult",resultCode);
+        paramMap.put("queryColumn",queryColumn);
+        paramMap.put("queryLimit",queryLimit);
+
         try {
-            return (Map<String,String>)dao.findForObject("commonMapper.findByType",type);
+            dao.update("commonMapper.updateStatus",paramMap);
+            result = true;
         } catch (Exception e) {
             e.printStackTrace();
+            result = false;
         }
-        return null;
+        return result;
+    }
+
+    /**
+     * 根据条件删除记录的公用方法
+     * @param tableName
+     * @param queryColumn
+     * @param queryLimit
+     * @return
+     */
+    public boolean deleteByCondition(String tableName,String queryColumn,String queryLimit) {
+        boolean result = false;
+        Map<String,String> paramMap = new HashMap<String, String>();
+        paramMap.put("tableName",tableName);
+        paramMap.put("queryColumn",queryColumn);
+        paramMap.put("queryLimit",queryLimit);
+
+        try {
+            dao.delete("commonMapper.deleteByCondition",paramMap);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
     }
 }
